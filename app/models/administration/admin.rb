a7fffb14deb904aa0d999942e12fa89b8d0ffa94
alxl <alx.l@gmx.com>
2014-04-03 12:10:43 +0000

    Minor fix

--- app/models/administration/admin.rb
+++ app/models/administration/admin.rb
@@ -1,8 +1,5 @@
 module Administration
   class Admin < ActiveRecord::Base
-    # Virtual attribute for email or username while signing in
-    attr_accessor :email
-
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :rememberable, :trackable, :validatable, :authentication_keys => [:email]
@@ -10,6 +7,6 @@ module Administration
     # Non-devise field validaitons
     validates :first_name, presence: true, length: { maximum: 20 }
     validates :last_name, presence: true, length: { maximum: 20 }
-    validates :username, presence: true, length: { maximum: 20 }, uniqueness: {case_sensitive: false}, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+    #validates :username, presence: true, length: { maximum: 20 }, uniqueness: {case_sensitive: false}, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   end
 end
